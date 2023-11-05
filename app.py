5bed332c4d9b7bbeee7c2e9567d8863d7f0f7287
semgoSE <semgoSE@gmail.com>
2023-11-06 00:04:06 +0800

    d

--- app.py
+++ app.py
@@ -9,35 +9,43 @@ compute_type = "int8"
 
 app = Flask(__name__)
 
-model = whisperx.load_model("medium", device, compute_type=compute_type)
+model = whisperx.load_model("small", device, compute_type=compute_type)
 
-@app.route('/')
+@app.route('/', methods = ["POST"])
 def main():
     content = request.json
     
-    # apiKey = request.headers.get("Authorization")
+    print(content)
     
-    # if (apiKey != "maNTAmbrOpto"):
-    #     return "Bad API key"
+    apiKey = request.headers.get("Authorization")
     
-    # r = requests.get(content.fileUrl, allow_redirects=True)
-    # open('audio.wav', 'wb').write(r.content)
+    if (apiKey != "maNTAmbrOpto"):
+        return "Bad API key"
+    
+    r = requests.get(content['fileUrl'], allow_redirects=True)
+    open('audio.wav', 'wb').write(r.content)
 
     audio = whisperx.load_audio(audio_file)
-    result = model.transcribe(audio, batch_size=1)
+    result = model.transcribe(audio, batch_size=1, language=content['language'])
     print(result["segments"])
 
     model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
     result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
     print(result["segments"])
     
-    diarize_model = whisperx.DiarizationPipeline(use_auth_token="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", device=device)
+    def only_text(segment): 
+        return segment.text
+    
+    texts = map(only_text, result["segments"])
+    
+    return jsonify({ text:  " ".join(texts)})
+    
+    # diarize_model = whisperx.DiarizationPipeline(use_auth_token="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", device=device)
 
-    diarize_segments = diarize_model(audio)
+    # diarize_segments = diarize_model(audio)
 
-    result = whisperx.assign_word_speakers(diarize_segments, result)
-    print(diarize_segments)
-    print(result["segments"])    
+    # result = whisperx.assign_word_speakers(diarize_segments, result)
+    # print(diarize_segments)
+    # print(result["segments"])    
     
     return 'Hello, World!'
-app.run()
