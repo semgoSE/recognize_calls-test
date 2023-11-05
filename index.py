bd2c8c6da4fca8f9627d7f152c47cfe8a135840c
semgoSE <semgoSE@gmail.com>
2023-11-05 21:21:42 +0800

    ff

--- index.py
+++ index.py
@@ -1,26 +1,33 @@
 from faster_whisper import WhisperModel
 import time
+import subprocess
+import os;
 
-model_size = "medim"
+path = "audio.wav"
+model_size = "medium"
 
-# Run on GPU with FP16
+nemo_process = subprocess.Popen(
+    ["python3", "nemo_process.py", "-a", path, "--device", "cuda"],
+)
 model = WhisperModel(model_size, device="cuda", compute_type="float16")
 
-# or run on GPU with INT8
-# model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
-# or run on CPU with INT8
-# model = WhisperModel(model_size, device="cpu", compute_type="int8")
-
-start_time = time.time()
-
-segments, info = model.transcribe("audio.wav", beam_size=5)
+segments, info = model.transcribe(path, beam_size=1, best_of=3)
 
 print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
 
 for segment in segments:
     print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
     
-end_time = time.time()
- 
-elapsed_time = end_time - start_time
-print('Elapsed time: ', elapsed_time)
+    
+# nemo_process.communicate()
+# ROOT = os.getcwd()
+# temp_path = os.path.join(ROOT, "temp_outputs")
+
+# speaker_ts = []
+# with open(os.path.join(temp_path, "pred_rttms", "mono_file.rttm"), "r") as f:
+#     lines = f.readlines()
+#     for line in lines:
+#         line_list = line.split(" ")
+#         s = int(float(line_list[5]) * 1000)
+#         e = s + int(float(line_list[8]) * 1000)
+#         speaker_ts.append([s, e, int(line_list[11].split("_")[-1])])
